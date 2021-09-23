1c0496812dae7e8dc81b22434266db84c0123db8
João Ferreira <joaovtf39@gmail.com>
2021-09-23 19:04:25 -0300

    requisito 8 OK

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Carregando from '../pages/Carregando';
 
 class MusicCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      getBackFavSOng: [],
+
+    };
+    this.keepFavoriteSongs = this.keepFavoriteSongs.bind(this);
+  }
+
+  async keepFavoriteSongs(track) {
+    this.setState({
+      loading: true,
+    }, async () => {
+      await addSong(track);
+      const keep = await getFavoriteSongs();
+      this.setState({
+        loading: false,
+        getBackFavSOng: keep,
+      });
+    });
+  }
+
   render() {
     const { storeMusic } = this.props;
+    const { loading, getBackFavSOng } = this.state;
+    const { keepFavoriteSongs } = this;
+    if (loading) return <Carregando />;
+    console.log(getBackFavSOng);
     return (
       <div>
         {storeMusic.slice(1).map(({ trackName, previewUrl, trackId }) => (
@@ -15,6 +44,16 @@ class MusicCard extends Component {
               <code>audio</code>
               .
             </audio>
+            <label htmlFor={ `favorite-${trackId}` }>
+              Favorita
+              <input
+                onChange={ () => keepFavoriteSongs(trackId) }
+                id={ `favorite-${trackId}` }
+                checked={ getBackFavSOng.some((t) => t === trackId) }
+                type="checkbox"
+                data-testid={ `checkbox-music-${trackId}` }
+              />
+            </label>
           </div>
         ))}
       </div>
